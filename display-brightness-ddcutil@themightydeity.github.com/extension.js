01570cf8a141b15dcd5dbc72361bcdce4c7fd66e
Binay Devkota <devunderscorebinay@gmail.com>
2020-03-11 04:41:50 +0200

    async try

--- display-brightness-ddcutil@themightydeity.github.com/extension.js
+++ display-brightness-ddcutil@themightydeity.github.com/extension.js
@@ -6,7 +6,7 @@
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * but WITHOUT ANY WARRANTY without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
@@ -16,30 +16,32 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
-const Main = imports.ui.main;
-const ByteArray = imports.byteArray;
-const { Clutter, GObject, St } = imports.gi;
+const Main = imports.ui.main
+const ByteArray = imports.byteArray
+const { Clutter, GObject, St } = imports.gi
 
 // for shell command 
-const GLib = imports.gi.GLib;
+const GLib = imports.gi.GLib
 
 // icons and labels
-const Lang = imports.lang;
+const Lang = imports.lang
 
 // menu items
-const Panel = imports.ui.panel;
+const Panel = imports.ui.panel
 const PanelMenu = imports.ui.panelMenu
-const PopupMenu = imports.ui.popupMenu;
-const Slider = imports.ui.slider;
+const PopupMenu = imports.ui.popupMenu
+const Slider = imports.ui.slider
 
-let panelmenu, icon;
-let brightnessIcon = 'display-brightness-symbolic';
+let panelmenu, icon
+let brightnessIcon = 'display-brightness-symbolic'
 
 /* lowest possible value for brightness */
-const minBrightness = 1;
+const minBrightness = 1
 
 /* when should min brightness value should be used */
-const minBrightnessThreshold = 5;
+const minBrightnessThreshold = 5
+
+let displays = []
 
 /* exported init */
 
@@ -57,15 +59,15 @@ class Extension {
 }
 
 function init() {
-    return new Extension();
+    return new Extension()
 }
 
 function spawnCommandAndRead(command_line) {
     try {
-        let stuff = ByteArray.toString(GLib.spawn_command_line_sync(command_line)[1]);
-        return stuff;
+        let stuff = ByteArray.toString(GLib.spawn_command_line_sync(command_line)[1])
+        return stuff
     } catch (err) {
-        return null;
+        return null
     }
 }
 const SliderMenuItem = GObject.registerClass(
@@ -73,117 +75,151 @@ const SliderMenuItem = GObject.registerClass(
         GType: 'SliderMenuItem'
     }, class SliderMenuItem extends PopupMenu.PopupMenuItem {
     _init(slider) {
-        super._init("");
-        this.add_child(slider);
+        super._init("")
+        this.add_child(slider)
     }
-});
+})
 
 const SliderPanelMenuButton = GObject.registerClass(
     {
         GType: 'SliderPanelMenuButton'
     }, class SliderPanelMenuButton extends PanelMenu.Button {
-        _init() {
-            super._init(0.0);
-            icon = new St.Icon({ icon_name: brightnessIcon, style_class: 'system-status-icon' });
-            this.add_actor(icon);
-        }
-        removeAllMenu(){
-            this.menu.removeAll();
-        }
-        addMenuItem(item){
-            this.menu.addMenuItem(item);
-        }
-});
+    _init() {
+        super._init(0.0)
+        icon = new St.Icon({ icon_name: brightnessIcon, style_class: 'system-status-icon' })
+        this.menu.connect('open-state-changed', this._onOpenStateChanged.bind(this))
+        this.add_actor(icon)
+    }
+    removeAllMenu() {
+        this.menu.removeAll()
+    }
+    addMenuItem(item) {
+        this.menu.addMenuItem(item)
+    }
+    _onOpenStateChanged(menu, open) {
+        updateSliderForAllDisplays()
+    }
+})
 
 class SliderItem extends PopupMenu.PopupMenuSection {
-    constructor(displayName, currentValue, onSliderChange) {
-        super();
-        this._displayName = displayName;
-        this._currentValue = currentValue;
-        this._onSliderChange = onSliderChange;
-        this._init();
+    constructor(display, onSliderChange) {
+        super()
+        this._onSliderChange = onSliderChange
+        this._init(display)
     }
-    _init() {
-        this.NameContainer = new PopupMenu.PopupMenuItem(this._displayName, { hover: false, reactive: false, can_focus: false });
+    _init(display) {
+        this.NameContainer = new PopupMenu.PopupMenuItem(display.name, { hover: false, reactive: false, can_focus: false })
 
-        this.ValueSlider = new Slider.Slider(this._currentValue);
-        this.ValueSlider.connect('notify::value', Lang.bind(this, this._SliderChange));
+        this.ValueSlider = new Slider.Slider(display.current / display.max)
+        this.ValueSlider.connect('notify::value', () => this._SliderChange())
+        display.slider = this.ValueSlider
 
-        this.SliderContainer = new SliderMenuItem(this.ValueSlider);
+        this.SliderContainer = new SliderMenuItem(this.ValueSlider)
 
         // add Slider to it
-        this.addMenuItem(this.NameContainer);
-        this.addMenuItem(this.SliderContainer);
-        this.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+        this.addMenuItem(this.NameContainer)
+        this.addMenuItem(this.SliderContainer)
+        this.addMenuItem(new PopupMenu.PopupSeparatorMenuItem())
     }
     _SliderChange() {
-        let sliderval = Math.floor(this.ValueSlider.value * 100);
-        this._onSliderChange(sliderval);
+        let sliderval = parseInt(this.ValueSlider.value * 100)
+        this._onSliderChange(sliderval)
     }
+}
+function updateSliderForAllDisplays(){
+    /* fetch the current brightness state of monitors and update slider when menu is closed */
+    displays.forEach(function(display) {
+        updateSliderWithCurrentBrightnessValue(display).then((display)=>{
+            /* function to fetch current brightness 
+            from monitor the and populate the slider */
+            display.triggerUpdateToDevice = false
+            display.slider.value = (display.current / display.max)
+            display.triggerUpdateToDevice = true
+        },()=>{
+
+        })
+    })
+}
+function updateSliderWithCurrentBrightnessValue(display) {
+    return new Promise((resolve, reject) => {
+        if (display.slider) {
+            getDisplayBrightness(display)
+            resolve(display)
+        }else{
+            reject(display)
+        }
+    })
+}
+function getDisplayBrightness(display) {
+    /* read the current and max brightness using getvcp 10 */
+    let vcpInfos = spawnCommandAndRead("ddcutil getvcp --nodetect --brief 10 --bus " + display.bus)
+    let vcpInfosArray = vcpInfos.trim().split(" ")
+    let maxBrightness = parseInt(vcpInfosArray[4])
+    let currentBrightness = parseInt((parseInt(vcpInfosArray[3]) / maxBrightness) * 100)
+    display.max = maxBrightness
+    display.current = currentBrightness
 }
 
 function getDisplays() {
-    let displays = [];
-    let ddc_output = spawnCommandAndRead("ddcutil detect --brief");
-    if (ddc_output && ddc_output !== "") {
-        let ddc_lines = ddc_output.split('\n');
-        let display = {};
-        for (let i = 0; i < ddc_lines.length; i++) {
-            let ddc_line = ddc_lines[i];
-            if (ddc_line.indexOf("/dev/i2c-") !== -1) {
-                /* I2C bus comes first, so when that is detect start a new display object */
-                let display_bus = ddc_line.split("/dev/i2c-")[1].trim();
-
-                /* read the current and max brightness using getvcp 10 */
-                let vcpInfos = spawnCommandAndRead("ddcutil getvcp --nodetect --brief 10 --bus " + display_bus);
-                let vcpInfosArray = vcpInfos.trim().split(" ");
-                let maxBrightness = vcpInfosArray[4];
-                /* we need current brightness in the scale of 0 to 1 for slider*/
-                let currentBrightness = vcpInfosArray[3] / vcpInfosArray[4];
-
-                /* make display object */
-                display = { "bus": display_bus, "max": maxBrightness, "current": currentBrightness };
-            }
-            if (ddc_line.indexOf("Monitor:") !== -1) {
-                /* Monitor name comes second, so when that is detected fill the object and push it to list */
-                display["name"] = ddc_line.split("Monitor:")[1].trim().split(":")[1].trim()
-                displays.push(display)
+    displays = []
+    return new Promise((resolve, reject) => {
+        let ddc_output = spawnCommandAndRead("ddcutil detect --brief")
+        if (ddc_output && ddc_output !== "") {
+            let ddc_lines = ddc_output.split('\n')
+            let display = {}
+            for (let i = 0; i < ddc_lines.length; i++) {
+                let ddc_line = ddc_lines[i]
+                if (ddc_line.indexOf("/dev/i2c-") !== -1) {
+                    /* I2C bus comes first, so when that is detect start a new display object */
+                    let display_bus = ddc_line.split("/dev/i2c-")[1].trim()
+                    /* make display object */
+                    display = { bus: display_bus, max: null, current: null, slider: null, triggerUpdateToDevice: true, name: "" }
+                    getDisplayBrightness(display)
+                }
+                if (ddc_line.indexOf("Monitor:") !== -1) {
+                    /* Monitor name comes second, so when that is detected fill the object and push it to list */
+                    display.name = ddc_line.split("Monitor:")[1].trim().split(":")[1].trim()
+                    displays.push(display)
+                }
             }
         }
-    }
-    return displays;
+        if (displays.length > 0) {
+            global.log("found displays 1")
+            resolve(displays)
+        } else {
+            reject()
+        }
+    })
 }
 function setBrightness(display, newValue) {
-    let newBrightness = parseInt((newValue / 100) * display.max);
-    if (newBrightness <= minBrightnessThreshold) {
-        newBrightness = minBrightness;
+    if (display.triggerUpdateToDevice) {
+        let newBrightness = parseInt((newValue / 100) * display.max)
+        if (newBrightness <= minBrightnessThreshold) {
+            newBrightness = minBrightness
+        }
+        GLib.spawn_command_line_async("ddcutil setvcp 10 " + newBrightness + " --nodetect --bus " + display.bus)
     }
-    global.log(display.name, newValue, newBrightness)
-    GLib.spawn_command_line_async("ddcutil setvcp 10 " + newBrightness + " --nodetect --bus " + display.bus)
 }
-
-
 function SliderPanelMenu(set) {
     if (set == "enable") {
         panelmenu = new SliderPanelMenuButton()
-        Main.panel.addToStatusArea("BrightnessSlider", panelmenu, 0, "right");
-        panelmenu.removeAllMenu();
-        let displays = getDisplays();
-        if (displays.length > 0) {
-            displays.forEach(function (display) {
+        Main.panel.addToStatusArea("BrightnessSlider", panelmenu, 0, "right")
+        panelmenu.removeAllMenu()
+        getDisplays().then((displays) => {
+            global.log("found displays 3")
+            displays.forEach((display) => {
                 let onSliderChange = function (newValue) {
                     setBrightness(display, newValue)
-                };
-                let displaySlider = new SliderItem(display.name, display.current, onSliderChange)
-                panelmenu.addMenuItem(displaySlider);
-            });
-        } else {
+                }
+                let displaySlider = new SliderItem(display, onSliderChange)
+                panelmenu.addMenuItem(displaySlider)
+            })
+        }, () => {
             global.error("ddcutil didn't find any display.")
-        }
-
+        })
     } else if (set == "disable") {
-        panelmenu.destroy();
-        panelmenu = null;
-        icon = null;
+        panelmenu.destroy()
+        panelmenu = null
+        icon = null
     }
 }
